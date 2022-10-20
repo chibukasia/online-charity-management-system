2e8a12a1889aa25cc4f60983d9b75b72b24b7926
Chibukasia Nelson <chibukasianelson@gmail.com>
2022-10-20 22:46:13 +0300

    Sessions and cookies configuratios

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
 class ApplicationController < ActionController::API
-
+    include ActionController::Cookies
     wrap_parameters format: []
 end
--- config/application.rb
+++ config/application.rb
@@ -11,17 +11,13 @@ module OnlineCharityManagementSystem
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    # Sessions and cookies configuartions 
+    config.middleware.use ActionDispatch::Cookies 
+    config.middleware.use ActionDispatch::Session::CookieStore
 
-    # Only loads a smaller set of middleware suitable for API only apps.
-    # Middleware like session, flash, cookies can be added back manually.
-    # Skip views, helpers and assets when generating a new resource.
+    # Use SameSite=Strict for all cookies to help protect against CSRF
+    config.action_dispatch.cookies_same_site_protectio = :Strict
+    
     config.api_only = true
   end
 end
