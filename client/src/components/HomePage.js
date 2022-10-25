86c42b7b2f0f562ff3c5483924e840b0a30cc101
Titus <titus.kamunya@student.moringaschool.com>
2022-10-25 11:10:28 +0300

    adds homepage with all approved requests

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,9 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react"; 
+import DonationRequestCard from "./DonationRequestCard";
 
-function HomePage(){
-    return (
-        <div>
-            <h2>Home page</h2>
+function HomePage(){ 
+    // use states
+    const [approvedRequests, setApprovedRequests] = useState([])
+    const [errors, setErrors] = useState([])
+
+    // Get all approved requests
+    useEffect(()=>{
+        fetch("/approved_open_requests")
+        .then(res=>{
+            if (res.ok){
+                res.json().then(data=>setApprovedRequests(data))
+            }else{
+                res.json().then(err=>setErrors(err.errors))
+            }
+        })
+    },[])
+
+    // Map through the approved requests to the donation request cards 
+    const cardsDisplay = approvedRequests.map(request=>{
+        return <DonationRequestCard key={request.id} request={request} />
+    })
+    return(
+        <div className="landing-page">
+            
+            <h2>Ongoing Fundraises</h2>
+            <div className="landing-page-cards">
+                {cardsDisplay}
+            </div>
         </div>
     )
 }
