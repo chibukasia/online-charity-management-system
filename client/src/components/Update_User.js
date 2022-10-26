5791de9d92561605f4edd5fa8e5088918206d095
chibukasia <39667302+chibukasia@users.noreply.github.com>
2022-10-26 16:10:26 +0300

    Update Update_User.js

--- client/src/components/Update_User.js
+++ client/src/components/Update_User.js
@@ -5,29 +5,31 @@ import "./Profile.css"
 const EditUserProfile = () => {
     return ( 
       <div>
+        <form>
         <div className="form-floating">
-        <input type="name" className="form-control" id="floatingName" placeholder="Enter Name..." />
+        <input type="name" className="form-control" id="floatingName" value="" />
         <label htmlFor="floatingName">First Name</label>
         </div>
         <div className="form-floating">
-        <input type="name" className="form-control" id="floatingName" placeholder="Enter Name..." />
+        <input type="name" className="form-control" id="floatingName" value="" />
         <label htmlFor="floatingName">Second Name</label>
         </div>
       <div className="form-floating">
-        <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" />
+        <input type="email" className="form-control" id="floatingInput" value="" />
         <label htmlFor="floatingInput">Email address</label>
       </div>
       <div className="form-floating">
-        <input type="password" className="form-control" id="floatingPassword" placeholder="Password" />
+        <input type="password" className="form-control" id="floatingPassword" value="" />
         <label htmlFor="floatingPassword">Password</label>
         </div>
         <div className="form-floating">
-        <input type="password" className="form-control" id="floatingPassword" placeholder="Password" />
+        <input type="password" className="form-control" id="floatingPassword" value="" />
         <label htmlFor="floatingPassword">Password Confirmation</label>
         </div>
         <div className="form-floating">
-        <button type="button" >Update Details</button>
+        <button type="submit" >Update Details</button>
         </div>
+        </form>
 
     </div>
      );
