7543c0ef515bf566e532673cad1bb30f76f53ba7
Chibukasia Nelson <chibukasianelson@gmail.com>
2022-10-26 13:39:24 +0300

    updated css

--- client/src/components/RequestCard.js
+++ client/src/components/RequestCard.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function RequestCard({request}) {
   return (
-    <div className="card" >
+    <div className="card" id="card">
       <img src={request.image_url} className="card-img-top" alt={request.title} style={{height: "18rem"}} />
       <div className="card-body">
         <h5 className="card-title">{request.title}</h5>
